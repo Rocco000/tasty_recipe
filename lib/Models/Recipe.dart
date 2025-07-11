1d574e47172b3b46316b7d040eea31437ce7ecfb
Rocco Iuliano <rocco.iul2000@gmail.com>
2025-07-12 00:49:37 +0200

    Rocco000/tasty_recipe#1 Developed the Create Recipe Screens
    Developed the Recipe entity and CreateRecipeScreen class to create a new recipe and insert the general info

---
+++ lib/Models/Recipe.dart
@@ -0,0 +1,121 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class Recipe {
+  int _id;
+  File? _image;
+  String _name;
+  int _difficulty;
+  int _duration;
+  int _servings;
+  String _category;
+  List<String> _tags;
+  bool _favorite = false;
+
+  static final List<String> categoryList = [
+    "First Course",
+    "Second Course",
+    "Breakfast",
+    "Snack",
+    "Dessert",
+  ];
+
+  static final List<Widget> mealIcons = [
+    Icon(Icons.dinner_dining_outlined),
+    Image.asset("content/images/secondCourse.png", width: 25, height: 25),
+    Icon(Icons.bakery_dining_outlined),
+    Image.asset("content/images/snack.png", width: 25, height: 25),
+    Icon(Icons.cake_outlined),
+  ];
+
+
+  static final List<String> recipeTags = [
+    "Vegan",
+    "Gluten Free",
+    "Lactose Free",
+    "Fit",
+  ];
+
+  static final List<Widget> recipeTagIcons = [
+    Icon(Icons.eco, color: Colors.lightGreen),
+    SizedBox(
+      width: 25,
+      height: 25,
+      child: Image.asset(
+        "content/images/glutenFree.png",
+        // 0xFFFFE0B2
+        color: Colors.orange[200],
+        fit: BoxFit.contain,
+      ),
+    ),
+    SizedBox(
+      width: 25,
+      height: 25,
+      child: Image.asset(
+        "content/images/lactoseFree.png",
+        color: Colors.lightBlue[700],
+        fit: BoxFit.contain,
+      ),
+    ),
+    Icon(Icons.fitness_center_outlined, color: Colors.grey[600]),
+  ];
+
+  Recipe(
+    this._image,
+    this._id,
+    this._name,
+    this._difficulty,
+    this._duration,
+    this._servings,
+    this._category,
+    this._tags,
+    this._favorite,
+  );
+
+  static Widget getMealIcon(String mealName){
+    if (!categoryList.contains(mealName))
+      throw Exception("The input meal category doesn't exist!");
+    
+    return mealIcons[categoryList.indexOf(mealName)];
+  }
+
+  static Widget getTagIcon(String tagName) {
+    if (!recipeTags.contains(tagName))
+      throw Exception("The input tag doesn't exist!");
+
+    int index = recipeTags.indexOf(tagName);
+    return recipeTagIcons[index];
+  }
+
+  File? get image => _image;
+  
+  set image(File? newImage) => _image = newImage;
+
+  String get name => _name;
+
+  set name(String newName) => _name = newName;
+
+  int get difficulty => _difficulty;
+
+  set difficulty(int newDifficulty) => _difficulty = newDifficulty;
+
+  int get duration => _duration;
+
+  set duration(int newDuration) => _duration = newDuration;
+
+  int get servings => _servings;
+
+  set servings(int newServings) => _servings = newServings;
+
+  String get category => _category;
+
+  set category(String newCategory) => _category = newCategory;
+
+  List<String> get tags => _tags;
+
+  void addTag(String tag) => _tags.add(tag);
+
+  bool get isFavorite => _favorite;
+
+  void changeFavoriteState() => _favorite = !_favorite;
+}
---
+++ lib/Screens/CreateRecipeScreen.dart
@@ -0,0 +1,134 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tasty_recipe/Models/Recipe.dart';
+import 'package:tasty_recipe/Widgets/RecipeGeneralInfoForm.dart';
+
+class CreateRecipeScreen extends StatefulWidget {
+  final String route = "/createRecipe";
+  const CreateRecipeScreen({super.key});
+
+  @override
+  State<CreateRecipeScreen> createState() => _CreateRecipeScreenState();
+}
+
+class _CreateRecipeScreenState extends State<CreateRecipeScreen> {
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  Recipe _newRecipe = Recipe(null, 0, "", 1, 1, 1, "First Course", [], false);
+  File? _pickedImg;
+  int _selectedChefHat = 0;
+
+
+  final List<bool> _selectedTags = [false, false, false, false];
+
+  Future _pickImageFromGallery() async {
+    // source: indicate where it should pick the image
+    final img = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    // WHY???
+    if (img != null) {
+      setState(() {
+        _pickedImg = File(img.path);
+      });
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          foregroundColor: Colors.white,
+          title: const Text("Recipe App"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: <Widget>[
+              // LOGO + TEXT
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "content/images/cooking.png",
+                      width: 100,
+                      height: 100,
+                    ),
+                    const Text(
+                      "Create a new recipe!",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Recipe Form
+              RecipeGeneralInfoForm(
+                existingRecipe: null,
+                formKey: _formKey,
+                recipeImg: _pickedImg,
+                onImageUploaded: _pickImageFromGallery,
+                difficultyLevel: _selectedChefHat,
+                onDifficultyChanged: (difficulty){
+                  setState(() {
+                    _newRecipe.difficulty = difficulty;
+                    _selectedChefHat = difficulty;
+                  });
+                },
+                selectedTags: _selectedTags,
+                onTagChanged: (tagName, selection){
+                  setState(() {
+                    _selectedTags[Recipe.recipeTags.indexOf(tagName)] = selection;
+                    if (_newRecipe.tags.contains(tagName)){
+                      _newRecipe.tags.remove(tagName);
+                    }
+                    else{
+                      _newRecipe.tags.add(tagName);
+                    }
+                  });
+                }
+              ),
+
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.saveAndValidate()) {
+                    final formFields = _formKey.currentState!.value;
+                    var app = {
+                      "name": formFields["recipeName"],
+                      "duration": formFields["recipeDuration"],
+                      "difficulty": _newRecipe.difficulty,
+                      "servings": formFields["recipeServing"],
+                      "category": formFields["recipeCategory"],
+                      "tags": _newRecipe.tags
+                    };
+                    print(app);
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  foregroundColor: Colors.white,
+                  shadowColor: Colors.black,
+                  elevation: 2.0,
+                  //fixedSize: Size(5, 25),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(2),
+                  ),
+                ),
+                child: const Text("Continue"),
+              ),
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
