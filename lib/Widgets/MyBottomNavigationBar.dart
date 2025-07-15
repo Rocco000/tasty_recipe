98bf6d803129d3c4acae4d4fbf0e043a5ae6ea01
Rocco000 <r.iuliano13@studenti.unisa.it>
2025-07-15 12:48:30 +0200

    Developed a custom BottomNavigationBar

---
+++ lib/Widgets/MyBottomNavigationBar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:tasty_recipe/Screens/CartScreen.dart';
+import 'package:tasty_recipe/Screens/CreateRecipeScreen.dart';
+import 'package:tasty_recipe/Screens/HomeScreen.dart';
+import 'package:tasty_recipe/Screens/ProfileScreen.dart';
+import 'package:tasty_recipe/Screens/ShowRecipeListScreen.dart';
+
+class MyBottomNavigationBar extends StatelessWidget {
+  final int index;
+
+  const MyBottomNavigationBar(this.index, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: index,
+      elevation: 2,
+      selectedItemColor: Colors.orange,
+      unselectedItemColor: Colors.grey,
+      onTap: (value) {
+        switch (value) {
+          case 0:
+            Navigator.pushNamed(context, HomeScreen.route);
+          case 1:
+            Navigator.pushNamed(context, CartScreen.route);
+          case 2:
+            Navigator.pushNamed(context, CreateRecipeScreen.route);
+          case 3:
+            Navigator.pushNamed(context, ShowRecipeListScreen.route);
+          case 4:
+            Navigator.pushNamed(context, ProfileScreen.route);
+          default:
+            Navigator.pushNamed(context, HomeScreen.route);
+        }
+      },
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.home_filled),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.shopping_cart_sharp),
+          label: "Shopping list",
+        ),
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.add_box_rounded),
+          label: "Create Recipe",
+        ),
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.favorite),
+          label: "Favorite Recipes",
+        ),
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.person),
+          label: "Profile",
+        ),
+      ],
+    );
+  }
+}
